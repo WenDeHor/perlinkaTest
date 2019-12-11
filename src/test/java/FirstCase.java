9a54f3065a1147bbe7ed4d7b9077fb74e287b399
WenDeHor <gaheru20>
2019-12-11 09:50:35 +0200

    first work-5

--- src/test/java/FirstCase.java
+++ src/test/java/FirstCase.java
@@ -42,71 +42,28 @@ public class FirstCase extends WebDriverSetingss {
     }
 
     @Test
-    public void randomopen() {
-        openShues("a");
+    public void openShues() {
+        driver.get("https://www.perlinka.ua");
 
 
+        WebElement menu = driver.findElement(By.id("menu"));
+        menu.findElement(By.cssSelector("[href=\"/index.php/component/virtuemart/view/category/category_id/8/s/rasprodazha\"]")).click();
+
+        List<WebElement> listOfElements = driver.findElements(By.cssSelector("[class=\"item item__discont\"]"));
+        System.out.println(listOfElements.size());
+        driver.findElement(By.cssSelector("[class=\"item item__discont\"]")).click();
+
+
+        // driver.findElement(By.tagName("div")).click();
+        //driver.findElement(By.className("item__link")).findElements(By.tagName("div"));
+
+        //driver.quit();
     }
-
-    public void openShues(String shuesTitle) {
-        //WebElement e = driver.findElement(By.className("item__link"));
-        WebElement sum = driver.findElement(By.className("item item__discont"));
-        List<WebElement> list = driver.findElement(By.className("item__link")).findElements(By.className("item item__discont"));
-        for (WebElement shuse : list) {
-            WebElement href = shuse.findElement(By.tagName("a"));
-            if (href.getTagName().contains(shuesTitle)) {
-                href.click();
-                break;
-            }
-            System.out.println(list.size());
-        }
-
-
-        //TODO driver.findElement(By.cssSelector("[href=\"/index.php/component/virtuemart/view/productdetails/virtuemart_product_id/5004/demisezonnye_botinki_iz_nubuka,_na_lipuchkah\"]")).click();
-//WebDriverWait wait = new WebDriverWait(driver, 10);
-        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("menu")));
-
-        //driver.findElement(By.id("items-list clearfix")).click();
-//        int idproduct = 5000;
-//        Random random = new Random();
-//        int i = random.nextInt(10);
-//        int idP = idproduct;
-//        String url = ;
-
-//        WebElement menu2 = driver.findElement(By.id("item item__discont"));
-//        menu2.findElement(By.cssSelector("[href=\"/index.php/component/virtuemart/view/productdetails/virtuemart_product_id/5004/demisezonnye_botinki_iz_nubuka,_na_lipuchkah\"]")).click();
-
-//        WebElement botom = driver.findElement(By.cssSelector("items-list clearfix"));
-//        botom.findElement(By.id(url)).click();
+//    WebDriverWait wait = new WebDriverWait(driver, 30, 500);
 //
-//        String title = driver.findElement(By.cssSelector("item-details__old-price")).getText();
+//      wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("item__link")));
 
 
-        // WebElement heder = driver.findElement(By.id("[href=\"/index.php/component/virtuemart/view/category/category_id/8/s/rasprodazha\"]"));
-        //heder.findElement(By.cssSelector("[href=\"/index.php/component/virtuemart/view/category/category_id/8/s/rasprodazha\"]")).click();
-
-
-        //driver.findElement(By.className("[class=\"item item__discont\"]")).click();
-
-        // String title = driver.findElement(By.className("[class=\"item item__discont\"]")).getText();
-
-//        Assert.assertEquals(title, "item-details__old-price");
-
-
-        // driver.findElement(By.cssSelector("href=\"/index.php/component/virtuemart/view/productdetails/virtuemart_product_id/5005/demisezonnye_botinki_na_rebristoy_podoshve\""));
-
-        //WebDriverWait wait2 = new WebDriverWait(driver, 10);
-//        driver.findElement(By.cssSelector(url));
-//
-        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-
-        // WebElement title = driver.findElement(By.cssSelector("[item-details__old-price]"));
-        //String title = driver.getTitle();
-
-        //Assert.assertTrue(title.equals("[item-details__old-price]"));
-
-    }
 }
 
 
